49905dac6931bc0981f947cfe77d89a80a331fb3
Portik-Andrea <portik.andrea@student.ms.sapientia.ro>
2022-11-14 12:20:02 +0200

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component'
-import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { UserdashboardComponent } from './userdashboard/userdashboard.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent},
   { path: 'userdashboard', component: UserdashboardComponent},
   { path: '**', component: AppComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserdashboardComponent } from './userdashboard/userdashboard.component';
@@ -13,7 +12,6 @@ import { UserdashboardComponent } from './userdashboard/userdashboard.component'
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
     HeaderComponent,
     FooterComponent,
     UserdashboardComponent
