0f7a50c6e41bbd85787280811654fd85603e5d7d
Portik-Andrea <portik.andrea@student.ms.sapientia.ro>
2022-11-21 12:17:00 +0200

    routing add upload, typedata component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { AppComponent } from './app.component';
 import { UserdashboardComponent } from './userdashboard/userdashboard.component';
+import { TypedataComponent } from './userdashboard/typedata/typedata.component';
+import { UploadComponent } from './userdashboard/upload/upload.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'userdashboard', component: UserdashboardComponent},
+  { path: 'userdashboard/typedata', component: TypedataComponent},
+  { path: 'userdasboard/upload',component: UploadComponent },
   { path: '**', component: AppComponent}
 ];
 
