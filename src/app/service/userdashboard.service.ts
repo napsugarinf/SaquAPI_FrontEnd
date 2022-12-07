72ac7532fbf2dc53e072710ca411f8e4e6ea1738
napsugarinf <napsugarinf@gmail.com>
2022-12-07 17:15:29 +0200

    get room data added

--- src/app/service/userdashboard.service.ts
+++ src/app/service/userdashboard.service.ts
@@ -15,31 +15,19 @@ export class UserdashboardService {
  // `${environment.apiUrl}/dashboard/user/change-password`
   constructor(private http: HttpClient) { }
 
-  uploadPhoto(file : File):Observable<any> {
-    const formData = new FormData();
-  // //uploadURL = 'http://{{ip}}:8080/saquapi/api/dashboard/user/save';
-  // const fd = new FormData();
-  // fd.append('image', file, file.name);
-  // this.http.post('http://localhost:8080/saquapi/api/dashboard/user/save', fd)
-  // .subscribe(res => {console.log(res)})
-        //(this.handleData)
-       // .catch(this.handleError);
-    // // Create form data
-    // const formData = new FormData(); 
-      
-    // // Store form name as "file" with file data
-    // formData.append("file", file, file.name);
-      
-    // // Make http post request over api
-    // // with formData as req
-     return this.http.post(this.uploadURL, formData)
-
-   }
-
-sendWaterMeterData(coldWaterValue?: Number):Observable<any>{
-  const headers ={ 'content-type': 'application/json'}
-  const body = JSON.stringify(coldWaterValue);
-  console.log(body);
-  return this.http.put<any>(`${environment.apiUrl}/dashboard/user/`, body,{'headers': headers})
+photoUpload(){
+  
 }
+
+// sendWaterMeterData(coldWaterValue?: Number):Observable<any>{
+//   const headers ={ 'content-type': 'application/json'}
+//   const body = JSON.stringify(coldWaterValue);
+//   console.log(body);
+//   return this.http.put<any>(`${environment.apiUrl}/dashboard/user/`, body,{'headers': headers})
+// }
+getRoomData(){
+  console.log('get roomData data');
+  return this.http.get<RoomData[]>(`${environment.apiUrl}/dashboard/admin/get-room-data?roomNumber=100`)
+}
+
 }
--- src/app/userdashboard/upload/upload.component.ts
+++ src/app/userdashboard/upload/upload.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserdashboardService } from 'src/app/service/userdashboard.service'
+import { RoomData } from 'src/app/model/roomdata';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -8,41 +9,37 @@ import { UserdashboardService } from 'src/app/service/userdashboard.service'
 })
 export class UploadComponent implements OnInit {
 // Variable to store shortLink from api response
+
+roomData! : RoomData[];
+
 shortLink: string = "";
 loading: boolean = false; // Flag variable
 file: any = null; // Variable to store file
-  constructor( private userDashboardService: UserdashboardService) { }
+  constructor( private userDashboardService: UserdashboardService, 
+    private router: Router,
+    private apiService:UserdashboardService) { }
 
   ngOnInit(): void {
 
   }
-  // photoUploadHandler(): void{
-  //   this.router.navigateByUrl('/userdashboard')
-  // }
+
   // On file Select
   onChange(event: any) {
     this.file = event.target.files[0];
 }
 
-// OnClick of button Upload
-photoUploadHandler() {
-    this.loading = !this.loading;
-    console.log(this.file);
-    this.userDashboardService.uploadPhoto(this.file).subscribe(
-        (event: any) => {
-            if (typeof (event) === 'object') {
+// OnClick of button Submit
+ photoUploadHandler() {
 
-                // Short link via api response
-                this.shortLink = event.link;
+ }
 
-                this.loading = false; // Flag variable 
-            }
-        }
-    );
-}
-
-
-
-
+getRoomDataHandler() {
+        this.apiService.getRoomData()
+          .subscribe(data =>{
+            console.log(data)
+            this.roomData=data;
+          })
 
 }
+}
+
